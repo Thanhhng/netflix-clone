7d0687dede91f98458721bc583eff40180514450
Your Name <you@example.com>
2023-12-20 15:16:27 +0700

    Init and finished login background

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+function LoginPage() {
+  return (
+    <div className="relative h-full w-full bg-[url('/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover opacity-50">
+      
+    </div>
+  )
+}
+
+export default LoginPage
